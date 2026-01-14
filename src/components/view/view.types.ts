84b3aef02b9b7ed244799f237f59cb796e8ed9c4
Elena Vlasenko <radionova-elena@mail.ru>
2026-01-14 13:09:14 +0300

    types view add

---
+++ src/components/view/view.types.ts
@@ -0,0 +1,47 @@
+import type { ResultInfo } from '../presenters/types';
+import type { Quiz, QuizId } from '@/types/quiz';
+import type { Observable, Renderable, Renderer } from '@/types/base';
+import type {
+    AnsweredOptionViewData,
+    ErrorDescription,
+    OptionViewData,
+    Progress,
+    QuestionOptionsData,
+    QuizHeadViewData,
+} from '@/types/view';
+
+export type QuizCardViewEvents = {
+    ['start_quiz_click']: { quizId: QuizId };
+};
+
+export type QuizCardView = Observable<QuizCardViewEvents>
+  & Renderable
+  & Renderer<Quiz>;
+
+export type QuizzesView = Observable<QuizCardViewEvents>
+  & Renderer<QuizCardView[]>
+  & { clear(): void };
+
+export type QuizGameResultViewEvents = {
+    ['restart']: undefined;
+};
+
+export type QuizGameResultView = Renderer<ResultInfo> & Observable<QuizGameResultViewEvents>;
+
+export type QuizCardFactory = (quiz: Quiz) => QuizCardView;
+
+export type QuestionOptionsView = Renderer<QuestionOptionsData> & {
+    readonly element: HTMLElement;
+};
+
+export type OptionView = {
+    readonly element: HTMLElement;
+    renderOptions(data: OptionViewData): void;
+    renderAnsweredOptions(data: AnsweredOptionViewData): void;
+};
+
+export type ErrorView = Renderer<ErrorDescription>;
+
+export type QuizHeaderView = Renderer<QuizHeadViewData>;
+
+export type QuizProgressView = Renderer<Progress>;
