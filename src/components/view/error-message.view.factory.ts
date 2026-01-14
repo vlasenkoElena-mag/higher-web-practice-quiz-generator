991c34f677a4f951822047cce76bde5dd4dd9cfb
Elena Vlasenko <radionova-elena@mail.ru>
2026-01-14 13:09:40 +0300

    error view add

---
+++ src/components/view/error-message.view.factory.ts
@@ -0,0 +1,33 @@
+import type { Renderer } from '@/types/base';
+import type { ErrorDescription } from '@/types/view';
+import { getFirstElementOrFail } from '@/utils/dom-utils';
+
+export const createErrorView = (element: HTMLElement): Renderer<ErrorDescription> => {
+    const messageElement = getFirstElementOrFail('.toast__title', element);
+    const detailsElement = getFirstElementOrFail('.toast__message', element);
+    const hideButton = getFirstElementOrFail('.toast__action', element) as HTMLButtonElement;
+
+    const show = () => {
+        element.hidden = false;
+        element.classList.add('toast--visible');
+    };
+
+    const hide = () => {
+        element.hidden = true;
+        element.classList.remove('toast--visible');
+    };
+
+    const setDescription = ({ message, details }: ErrorDescription) => {
+        messageElement.textContent = message;
+        detailsElement.textContent = details;
+    };
+
+    hideButton.addEventListener('click', () => hide());
+
+    return {
+        render: (description: ErrorDescription) => {
+            setDescription(description);
+            show();
+        },
+    };
+};
